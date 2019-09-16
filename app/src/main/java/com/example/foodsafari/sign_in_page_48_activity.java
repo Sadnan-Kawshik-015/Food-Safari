e6857c5079d033317002ec6f50027524ab7565bc
muhammad <muhammadibnzia.24csedu.048.2@gmail.com>
2019-09-16 14:15:07 +0600

    login page and userbsign up page added

---
+++ app/src/main/java/com/example/foodsafari/sign_in_page_48_activity.java
@@ -0,0 +1,14 @@
+package com.example.foodsafari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_in_page_48_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in_page_48_activity);
+    }
+}
