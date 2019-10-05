cfc7e2283bc9e08645873ad6318862865d4905e0
muhammad <muhammadibnzia.24csedu.048.2@gmail.com>
2019-10-05 18:44:08 +0600

    Some backend

--- app/src/main/java/com/example/foodsafari/UserLoginPage.java
+++ app/src/main/java/com/example/foodsafari/UserLoginPage.java
@@ -2,13 +2,49 @@ package com.example.foodsafari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UserLoginPage extends AppCompatActivity {
+public class UserLoginPage extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText loginEmailEditText;
+    private EditText loginPasswordEditText;
+    private Button userLoginButton;
+    private Button userSignUpButton;
+    private Button backButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login_page);
+
+        loginEmailEditText = findViewById(R.id.loginUserEmail);
+        loginPasswordEditText = findViewById(R.id.loginUserPassword);
+        userLoginButton = findViewById(R.id.userLoginButton);
+        userSignUpButton = findViewById(R.id.userSignUpButton);
+
+        userLoginButton.setOnClickListener(this);
+        userSignUpButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public  void onClick(View v){
+        switch (v.getId()){
+            case R.id.userLoginButton:
+
+                break;
+
+            case R.id.userSignUpButton:
+
+                Intent intent = new Intent(getApplicationContext(),UserSignInPage.class);
+                startActivity(intent);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/foodsafari/UserSignInPage.java
+++ app/src/main/java/com/example/foodsafari/UserSignInPage.java
@@ -2,13 +2,53 @@ package com.example.foodsafari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UserSignInPage extends AppCompatActivity {
+public class UserSignInPage extends AppCompatActivity implements View.OnClickListener {
+
+    private Button userRegisterButton;
+
+    private EditText userNameEditText;
+    private EditText userEmailEditText;
+    private EditText userPhnoEditText;
+    private EditText userPasswordEditText;
+    private EditText userAcnoEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_in_page);
+
+        userNameEditText = findViewById(R.id.userNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        userPhnoEditText = findViewById(R.id.userPhnoEditText);
+        userPasswordEditText = findViewById(R.id.userPasswordEditText);
+        userAcnoEditText = findViewById(R.id.userAcnoEditText);
+
+        userRegisterButton = findViewById(R.id.userRegisterButton);
+
+        userRegisterButton.setOnClickListener(this);
+
+
+
     }
+
+
+    public  void onClick(View v){
+        switch (v.getId()){
+            case R.id.userRegisterButton:
+
+                break;
+
+
+        }
+
+    }
+
+
 }
