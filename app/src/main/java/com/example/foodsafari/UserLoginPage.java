c234f23add8681a2a6dab44fed300a4670bace87
muhammad <muhammadibnzia.24csedu.048.2@gmail.com>
2019-10-05 14:33:06 +0600

    backend of userlogin

--- app/src/main/java/com/example/foodsafari/UserLoginPage.java
+++ app/src/main/java/com/example/foodsafari/UserLoginPage.java
@@ -3,12 +3,57 @@ package com.example.foodsafari;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class UserLoginPage extends AppCompatActivity {
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
+
+public class UserLoginPage extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText loginEmailEditText;
+    private EditText loginPasswordEditText;
+    private Button loginButton;
+    private Button signUpButton;
+    private Button backButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login_page);
+
+        loginEmailEditText = findViewById(R.id.loginEditTextUserName);
+        loginPasswordEditText = findViewById(R.id.loginEditTextPassword);
+        loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        loginButton.setOnClickListener(this);
+        signUpButton.setOnClickListener(this);
+
     }
+
+        @Override
+        public void onClick(View v ){
+
+            switch (v.getId()){
+
+                case R.id.loginButton:
+
+                    break;
+
+
+                case R.id.signUpButton:
+
+                    break;
+
+
+
+            }
+
+        }
+
+
 }
