4513287f49e298462978664b7ba24d9aafee4052
muhammad <muhammadibnzia.24csedu.048.2@gmail.com>
2019-10-05 18:00:51 +0600

    Revert "backend of userlogin"
    This reverts commit c234f23add8681a2a6dab44fed300a4670bace87.
    
    :wq

--- app/src/main/java/com/example/foodsafari/UserLoginPage.java
+++ app/src/main/java/com/example/foodsafari/UserLoginPage.java
@@ -3,57 +3,12 @@ package com.example.foodsafari;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SearchView;
-
-public class UserLoginPage extends AppCompatActivity implements View.OnClickListener {
-
-    private EditText loginEmailEditText;
-    private EditText loginPasswordEditText;
-    private Button loginButton;
-    private Button signUpButton;
-    private Button backButton;
-
-
-
+public class UserLoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login_page);
-
-        loginEmailEditText = findViewById(R.id.loginEditTextUserName);
-        loginPasswordEditText = findViewById(R.id.loginEditTextPassword);
-        loginButton = findViewById(R.id.loginButton);
-        signUpButton = findViewById(R.id.signUpButton);
-
-        loginButton.setOnClickListener(this);
-        signUpButton.setOnClickListener(this);
-
     }
-
-        @Override
-        public void onClick(View v ){
-
-            switch (v.getId()){
-
-                case R.id.loginButton:
-
-                    break;
-
-
-                case R.id.signUpButton:
-
-                    break;
-
-
-
-            }
-
-        }
-
-
 }
