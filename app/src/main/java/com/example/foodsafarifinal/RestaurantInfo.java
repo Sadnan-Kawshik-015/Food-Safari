5a98e6137dfa526b2d3fd82a7f9ff3650056b6e1
Sadnan Kawshik <sadnan.kawshik9148@gmail.com>
2019-10-06 13:36:55 +0600

    Refined dashboard and owner info with front and backend work

--- app/src/main/java/com/example/foodsafarifinal/RestaurantInfo.java
+++ app/src/main/java/com/example/foodsafarifinal/RestaurantInfo.java
@@ -3,12 +3,28 @@ package com.example.foodsafarifinal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RestaurantInfo extends AppCompatActivity {
+    TextView owner_name,email,restaurant_name,location;
+    ImageView owner_pic;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_info);
+
+        owner_name=findViewById(R.id.tv_restaurant_owner_name);
+        owner_pic=findViewById(R.id.iv_restaurant_owner_image);
+        email=findViewById(R.id.tv_restaurant_owner_email);
+        restaurant_name=findViewById(R.id.tv_restaurant_owner_info_restaurant_name);
+        location=findViewById(R.id.tv_restaurant_owner_restaurant_location);
+
+    }
+    public void changeText(String input,TextView t)
+    {
+        t.setText(input);
     }
 }
