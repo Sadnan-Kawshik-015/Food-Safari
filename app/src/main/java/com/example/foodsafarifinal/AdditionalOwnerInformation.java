0b75e72aa178d0fe8bb24fc0d925d6f43ffe3540
Sadnan Kawshik <sadnan.kawshik9148@gmail.com>
2019-10-30 12:12:41 +0600

    Commit with User Menu Partial

---
+++ app/src/main/java/com/example/foodsafarifinal/AdditionalOwnerInformation.java
@@ -0,0 +1,14 @@
+package com.example.foodsafarifinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdditionalOwnerInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional_owner_information);
+    }
+}
---
+++ app/src/main/java/com/example/foodsafarifinal/OwnerShowMenu.java
@@ -0,0 +1,14 @@
+package com.example.foodsafarifinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerShowMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_show_menu);
+    }
+}
