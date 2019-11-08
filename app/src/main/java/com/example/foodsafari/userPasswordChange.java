57eca5551e14b62e97d9e0539ee40b236e7389ea
muhammad <muhammadibnzia.24csedu.048.2@gmail.com>
2019-11-08 11:34:29 +0600

    user profile works without firebase done

--- app/src/main/java/com/example/foodsafari/userPasswordChange.java
+++ app/src/main/java/com/example/foodsafari/userPasswordChange.java
@@ -2,13 +2,75 @@ package com.example.foodsafari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class userPasswordChange extends AppCompatActivity {
+public class userPasswordChange extends AppCompatActivity implements View.OnClickListener {
+
+    private Button confirmButton;
+
+    private EditText newPasswordEditText1;
+    private  EditText newPasswordEditText2;
+
+    private String newPassword1;
+    private String newPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_password_change);
+
+        newPasswordEditText1 = findViewById(R.id.editText4);
+        newPasswordEditText2 = findViewById(R.id.editText5);
+
+        confirmButton = findViewById(R.id.passwordChangeButton);
+
+
+
+    }
+
+    @Override
+    public  void onClick(View v){
+
+        switch (v.getId()){
+            case R.id.passwordChangeButton:
+
+                newPassword1 = newPasswordEditText1.getText().toString();
+                newPassword2 = newPasswordEditText2.getText().toString();
+
+                if(newPasswordEditText1.isEmpty() || newPasswordEditText2.isEmpty()){
+                Toast.makeText(userPasswordChange.this,"Please give your Password",Toast.LENGTH_SHORT).show();
+            }
+
+                else if(!newPasswordEditText1.equals(newPasswordEditText2)){
+                    Toast.makeText(userPasswordChange.this,"Passwords didn't match\n Try again",Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+                else{
+
+                    //firebase related work to update the user password
+
+                    Toast.makeText(userPasswordChange.this,"Your Password is updated",Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(),userProfile.class);
+                    startActivity(intent);
+                }
+
+
+
+
+                break;
+
+
+
+        }
+
     }
 }
--- app/src/main/java/com/example/foodsafari/userProfile.java
+++ app/src/main/java/com/example/foodsafari/userProfile.java
@@ -2,13 +2,106 @@ package com.example.foodsafari;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class userProfile extends AppCompatActivity {
+public class userProfile extends AppCompatActivity implements View.OnClickListener {
+
+
+    private TextView historytextView;
+    private Button passwordChangeButton;
+    private Button updateButton;
+
+    private EditText userNameEditText;
+    private EditText userEmailEditText;
+    private EditText userPhnoEditText;
+
+
+
+
+    private String name;
+    private String email;
+    private String phno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+
+        userNameEditText = findViewById(R.id.userProfileNameEditText);
+        userEmailEditText = findViewById(R.id.userEmailEditText);
+        userPhnoEditText = findViewById(R.id.userPhnoEditText);
+
+        passwordChangeButton = findViewById(R.id.userPasswordChangeButton);
+        updateButton = findViewById(R.id.button2);
+        historytextView = findViewById(R.id.userHistoryTextView);
+
+
+    }
+
+    @Override
+    public void onClick(View v){
+
+        switch(v.getId()){
+
+            case R.id.userPasswordChangeButton:
+
+                Intent intent = new Intent(getApplicationContext(),userPasswordChange.class);
+                startActivity(intent);
+
+                break;
+
+
+
+
+//button2 is update button
+            case R.id.button2:
+
+                //updates all the fields except email
+
+                name = userNameEditText.getText().toString();
+                phno = userPhnoEditText.getText().toString();
+                email = userEmailEditText.getText().toString();
+
+
+
+                //firebase related works to update the fields
+
+
+
+
+
+                Toast.makeText(userProfile.this,"Your Update is done",Toast.LENGTH_SHORT).show();
+
+                //take to new activity or whatever else
+
+                break;
+
+
+
+
+
+
+    //here the page is directed to my cart but later on will be directed to sadnan kaushiks my orders
+
+            case R.id.userHistoryTextView:
+
+                Intent intent1 = new Intent(getApplicationContext(),customerOrderList.class);
+                startActivity(intent1);
+
+
+                break;
+
+
+
+
+        }
+
     }
 }
